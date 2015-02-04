d2065a90c70028019dea50c1b1c17dad7b2b7f04
anton kropp <akropp@gmail.com>
2015-02-03 17:15:01 -0800

    adding sbt-assembly and being less lazy in main

--- build.sbt
+++ build.sbt
@@ -6,8 +6,6 @@ version := "1.0"
 
 scalaVersion := "2.11.5"
 
-
-
 libraryDependencies ++= Seq{
   "com.fasterxml.jackson.core" % "jackson-databind" % "2.5.0"
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,48 +1,54 @@
-import java.io.{FileWriter, BufferedWriter, File}
-
-import com.sun.tools.internal.ws.processor.util.DirectoryUtil
+import java.io.{BufferedWriter, File, FileWriter}
 
 import scala.io.{Codec, Source}
 
 
 object Main extends App {
-  
-  val tinyTypeDefFile = args(0)
+  def run(): Unit = {
+    if (args.length < 2) {
+      println("[tiny types definition file] [output package name]")
 
-  val outputPackage = args(1)
+      return
+    }
 
-  println(s"using $tinyTypeDefFile, $outputPackage")
-  
-  var (templatedTypes, templatedConversions) =
-    readFile(tinyTypeDefFile)
-      .map(x => {println(s"Processing $x"); x})
-      .map(x => TinyMaker(x))
-      .map({ case TinyAggregate(declaration, conversion) => (declaration, conversion) })
-      .unzip
 
-  val tinyTemplates = templatedTypes.mkString(System.lineSeparator())
+    val tinyTypeDefFile = args(0)
 
-  val tinyConversions = withObjectConversions(templatedConversions.mkString(System.lineSeparator()))
+    val outputPackage = args(1)
 
-  writeAsPackage(outputPackage, tinyTemplates, "TinyTypes.scala")
-  writeAsPackage(outputPackage, tinyConversions, "TinyConversions.scala")
+    println(s"using $tinyTypeDefFile, $outputPackage")
+
+    val (templatedTypes, templatedConversions) =
+      readFile(tinyTypeDefFile)
+        .map(x => {
+        println(s"Processing $x"); x
+      })
+        .map(x => TinyMaker(x))
+        .map({ case TinyAggregate(declaration, conversion) => (declaration, conversion)})
+        .unzip
+
+    val tinyTemplates = templatedTypes.mkString(System.lineSeparator())
+
+    val tinyConversions = withObjectConversions(templatedConversions.mkString(System.lineSeparator()))
+
+    writeAsPackage(outputPackage, tinyTemplates, "TinyTypes.scala")
+    writeAsPackage(outputPackage, tinyConversions, "TinyConversions.scala")
+
+    println("DONE!")
+  }
 
   def readFile(path: String): Seq[TinyTypeDefinition] = {
     val s = Source.fromFile(path)(Codec.UTF8)
 
-    val r = s.getLines().map(x => TinyTypeDefinition(x)).toSeq
-
-    //s.close()
-
-    r
+    s.getLines().map(x => TinyTypeDefinition(x)).toSeq
   }
 
-  def writeAsPackage(packageName : String, content: String, fileName : String): Unit ={
-    val dir = s"src/main/scala/${packageName.replace('.','/')}"
+  def writeAsPackage(packageName: String, content: String, fileName: String): Unit = {
+    val dir = s"src/main/scala/${packageName.replace('.', '/')}"
 
     val f = new File(s"$dir/$fileName")
 
-    if(!f.getParentFile.exists()) {
+    if (!f.getParentFile.exists()) {
       f.getParentFile.mkdirs()
     }
 
@@ -53,10 +59,12 @@ object Main extends App {
     writer.close()
   }
 
-  def withObjectConversions(content : String) = {
+  def withObjectConversions(content: String) = {
     s"""
 object Conversions{
 ${content.split(System.lineSeparator()).map(i => "    " + i.trim).mkString(System.lineSeparator())}
 }""".trim
   }
+
+  run()
 }
